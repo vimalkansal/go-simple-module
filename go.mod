13f32adb06a850c331d30576f23a651e75f3d6bc
Vimal Kansal <Vimal.Y.Kansal@nab.com.au>
2021-04-15 14:34:53 +1000

    initial

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vimalkansal/go-simple-module
+
+go 1.16
+
+require (
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sugarme/tokenizer v0.1.15 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
---
+++ simple.go
@@ -0,0 +1,18 @@
+package go_simple_module
+
+import (
+	"fmt"
+	"log"
+	"github.com/sugarme/tokenizer/pretrained"
+)
+
+func Preprocess() []string {
+	tk := pretrained.BertBaseUncased()
+	sentence := `The quick brown fox jumps over the [L A Z Y D O G]`
+	en,err := tk.EncodeSingle(sentence)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("tokens: %q\n", en.Toekns)
+	return en.Tokens
+}
